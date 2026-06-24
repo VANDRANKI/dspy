8a1a585d022d412e685d35ffb770b72e4bcd45ee
PRABHU KIRAN VANDRANKI <72809214+VANDRANKI@users.noreply.github.com>
2026-06-24 04:43:50 -0400

    chore: improve package metadata in __metadata__.py
    - Expand description to be more informative
    - Add __license__ field for PyPI classifier accuracy
    - Add __keywords__ list to improve discoverability

--- dspy/__metadata__.py
+++ dspy/__metadata__.py
@@ -2,7 +2,9 @@
 __name__="dspy"
 #replace_package_version_marker
 __version__="3.2.0"
-__description__="DSPy"
+__description__="DSPy: Programming with Foundation Models"
 __url__="https://github.com/stanfordnlp/dspy"
 __author__="Omar Khattab"
 __author_email__="okhattab@stanford.edu"
+__license__="MIT"
+__keywords__=["LLM", "language models", "prompting", "programming", "AI"]
